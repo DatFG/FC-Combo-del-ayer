8e693980e341dfc93a25577381177fefbb940ee2
DataFG <datafferneyguevara@gmail.com>
2024-08-05 17:48:35 -0500

    enlazando copyrigth a whatsapp

--- sonidos.js
+++ sonidos.js
@@ -1,12 +1,10 @@
+var audio = document.getElementById("himno");
 
-    var audio = document.getElementById("himno");
+function playAudio1() {
+  audio.play();
+}
 
-    function playAudio1() {
-      audio.play();
-    }
-
-    function pauseAudio() {
-      audio.pause();
-      audio.currentTime = 0; // Reset the audio to the beginning
-    }
-  
+function pauseAudio() {
+  audio.pause();
+  audio.currentTime = 0; // Reset the audio to the beginning
+}
